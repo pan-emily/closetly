49f468e4d229dd6321fbb99777cf989f82966903
thebridgetyang <108485224+thebridgetyang@users.noreply.github.com>
2023-03-17 22:32:43 -0700

    Update grant-permissions.sql

--- grant-permissions.sql
+++ grant-permissions.sql
@@ -14,6 +14,9 @@ GRANT SELECT ON closetly.* TO 'appclient'@'localhost';
 
 GRANT SELECT, UPDATE, INSERT, DELETE ON closetly.store_closet TO 'storeowner'@'localhost';
 GRANT SELECT, INSERT ON closetly.styled_outfits TO 'storeowner'@'localhost';
+GRANT SELECT ON closetly.permissions TO 'storeowner'@'localhost';
+GRANT SELECT ON closetly.user TO 'storeowner'@'localhost';
+GRANT SELECT ON closetly.clothes TO 'storeowner'@'localhost';
 
 -- personal stylists: should have select privileges on store, collab
 -- and personal closets, and should bave update and insert privileges
@@ -23,6 +26,10 @@ GRANT SELECT ON closetly.collab_closet TO 'stylist'@'localhost';
 GRANT SELECT ON closetly.personal_closet TO 'stylist'@'localhost';
 GRANT SELECT on closetly.store_closet TO 'stylist'@'localhost';
 GRANT SELECT, UPDATE, INSERT ON closetly.styled_outfits TO 'stylist'@'localhost';
+GRANT SELECT ON closetly.permissions TO 'stylist'@'localhost';
+GRANT SELECT ON closetly.user TO 'stylist'@'localhost';
+GRANT SELECT ON closetly.clothes TO 'stylist'@'localhost';
+
 
 -- regular people: should have all privileges on their own private
 -- closets, should be able to view and select from store closets,
@@ -30,8 +37,15 @@ GRANT SELECT, UPDATE, INSERT ON closetly.styled_outfits TO 'stylist'@'localhost'
 -- as create new outfits in styled_outfits 
 GRANT SELECT, UPDATE, INSERT, DELETE ON closetly.personal_closet TO 'personal'@'localhost';
 GRANT SELECT ON closetly.store_closet TO 'personal'@'localhost';
+GRANT SELECT ON closetly.clothes TO 'personal'@'localhost';
 GRANT SELECT, UPDATE ON closetly.collab_closet TO 'personal'@'localhost';
 GRANT SELECT, UPDATE, INSERT ON closetly.styled_outfits TO 'personal'@'localhost';
+GRANT SELECT ON closetly.permissions TO 'personal'@'localhost';
+GRANT SELECT ON closetly.user TO 'personal'@'localhost';
+GRANT SELECT ON closetly.clothes TO 'personal'@'localhost';
+
+GRANT EXECUTE ON FUNCTION borrow_item TO 'personal'@'localhost';
+
 
 FLUSH PRIVILEGES;
 
